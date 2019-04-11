bd01dc76c29278e1424fe01417b10d1812f8f9f2
Paul Standley <paulstandley1972@gmail.com>
2019-04-11 19:43:11 +0100

    Setup create react app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import iconMarker from './iconMarker.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={iconMarker} className="App-logo" alt="logo" />
+          <p>
+            Work in progress <code>() => null</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
